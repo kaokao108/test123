c50c7643d9215a9d20ac06830130b2733c048dd8
kaokao <st200530415@gmail.com>
2017-11-10 23:20:23 +0800

    zz

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var apiai = require('apiai');
 var request = require('request');
 // var cheerio = require("cheerio");
 // var getJSON = require('get-json');
-// var fs = require('fs'),
+var fs = require('fs'),
 
 
 
@@ -22,14 +22,6 @@ var bot = linebot({
   "channelAccessToken": "/QKwGAhryLW1sInAcRYt4/yTt5GjNLGoc0Bkq+nIeCRCYbo+VkmlH/uTYkuAfhZzifXb32JKocAs/ou2LAPZKLiBIIY4iS7UjA8pS4n1sGhQH3eFuKEydozt82J8aqOaPexn6OWgkASzVODOpHr2pAdB04t89/1O/w1cDnyilFU="
 }); // 連接line，驗證
 
-var timer;
-var pm = [];
-// _getJSON();
-// bot.on('message',function(event){
-// 	event.reply("gg");
-// });
-_japan();
-// _bot();
 
 var app = express(); //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
 var linebotParser = bot.parser();
@@ -42,72 +34,25 @@ var server = app.listen(process.env.PORT || 8080, function() {
 });
 
 
-const googleMapsClient = require('@google/maps').createClient({ key: AIzaSyDpgeIebm3tlD72A6a0OBNzRbVyaS6H6Bc })
-  const payload = {
-    origins,
-    destinations,
-    units: 'metric',
-    language: 'zh-TW'
-  }
-  const GoogleMapPromise = new Promise((resolve, reject) => {
-    googleMapsClient.distanceMatrix(payload, (err, res) => {
-      if (!err) {
-        console.log('Google Distance Matrix Response', JSON.stringify(res.json))
-        const distanceMatrix = res.json.rows[0].elements 
-        // ...
+function _fuck() {
+  request({
+    url: "http://blog.infographics.tw",
+    method: "GET"
+  }, function(e,r,b) {
+    if(e || !b) { return; }
+    var $ = cheerio.load(b);
+    var result = [];
+    var titles = $("li.item h2");
+    for(var i=0 ; i<titles.length ; i++) {
+      result.push($(titles[i]).text());
+      bot.on('message',function(event){
+          event.reply(titles);
+        });
 
-        resolve(distanceMatrix)
-      }
-    })
-  })
-
-getShowtimes(theaterId).then((showtime) => {
-      let showtime_info = {}
-      let st = new Showtime()
-      st.cinema = cinema.cinemaName
-      st.theater = theaterId
-      try {
-        showtime_info = JSON.stringify(showtime)
-      } catch(err) {
-        console.log(`${theaterId} error: ${err}`)
-      }
-      st.showtime_info = showtime_info
-      st.save((err) => {
-        if(err) {
-          ErrorLogger(res, err.message, 'Failed to create new showtime.')
-          console.log(`Save theater${_theaterId} into DB Error`)
-        } else {
-          console.log(`[${theaterId}] save success`)
-        }
-      })
-  })
-
-
-import Crawler from 'js-crawler'
-import Cheerio from 'cheerio'
-import _ from 'lodash'
-import Promise from 'promise'
-
-crawler.crawl({
-  url: `http://www.vscinemas.com.tw/visPrintShowTimes.aspx?cid=${_theaterId}&visLang=2`,
-
-  success: (page) => {
-    const html = page.content.toString()
-    const $ = Cheerio.load(html)
-    let tables = $('.PrintShowTimesFilm').parent().parent().parent().find('table')
-    let showtimes = []
-    _.map(tables, (table, idx) => {
-      let title = $(table).find('.PrintShowTimesFilm').text()
-        // ...
-
-    })
-    resolve(showtimes)
-  },
-  failure: (page) => {
-    console.log(`Get Showtimes Failed on theater: ${_theaterId}`)
-    reject([])
-  }
-})
+    }
+    // fs.writeFileSync("result.json", JSON.stringify(result));
+  });
+}
 
 // function _japan() {
 //   // clearTimeout(timer2);
