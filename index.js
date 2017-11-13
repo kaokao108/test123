30751e90a98ca5142a40ba3df072de6567788b4c
kaokao <st200530415@gmail.com>
2017-11-13 17:43:32 +0800

    Z

--- index.js
+++ index.js
@@ -34,6 +34,19 @@ var server = app.listen(process.env.PORT || 8080, function() {
 });
 
 
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+    event.reply(msg).then(function(data) {
+      // success 
+      console.log(msg);
+    }).catch(function(error) {
+      // error 
+      console.log('error');
+    });
+  }
+});
+
 // function _fuck() {
 //   request({
 //     url: "http://blog.infographics.tw",
@@ -82,30 +95,30 @@ var server = app.listen(process.env.PORT || 8080, function() {
 //   });
 // }
 
-function _japan() {
-  // clearTimeout(timer2);
-  request({
-    url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
-    method: "GET"
-  }, function(error, response, body) {
-    if (error || !body) {
-      return;
-    } else {
-      var $ = cheerio.load(body);
-      var target = $(".rate-content-sight.text-right.print_hide");
-      // console.log(target[14].children[0].data);
-      var jp = target[14].children[0].data;
-      var jp2 = target[0].children[0].data;
-      // if (jp > 0) {
-        bot.on('message',function(event){
-          event.reply('現在日幣匯率' + jp +'美金' +jp2);
-        });
-        // bot.reply('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
-      // }
-      // timer2 = setInterval(_japan, 120000);
-    }
-  });
-}
+// function _japan() {
+//   // clearTimeout(timer2);
+//   request({
+//     url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
+//     method: "GET"
+//   }, function(error, response, body) {
+//     if (error || !body) {
+//       return;
+//     } else {
+//       var $ = cheerio.load(body);
+//       var target = $(".rate-content-sight.text-right.print_hide");
+//       // console.log(target[14].children[0].data);
+//       var jp = target[14].children[0].data;
+//       var jp2 = target[0].children[0].data;
+//       // if (jp > 0) {
+//         bot.on('message',function(event){
+//           event.reply('現在日幣匯率' + jp +'美金' +jp2);
+//         });
+//         // bot.reply('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
+//       // }
+//       // timer2 = setInterval(_japan, 120000);
+//     }
+//   });
+// }
 
 // function _bot() {
 //   bot.on('message', function(event) {
